3dc391b6dee2c9d9e15157a7ed407130f26cf060
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2024-04-15 12:57:00 -0600

    Sesión 34 - 15/04/2024

--- 03-Nodejs/04-ClimaApp/modelos/busquedas.js
+++ 03-Nodejs/04-ClimaApp/modelos/busquedas.js
@@ -7,9 +7,17 @@ class Busquedas {
 
     }
 
-    async ciudad( lugar ) {
-        const { data } = await axios.get('https://reqres.in/api/users?page=2');
-        console.log(data);
+    async ciudad( lugar = '' ) {
+        const consulta = axios.create({
+            baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+            params: {
+                limit: 5,
+                language: 'es',
+                'access_token': 'pk.eyJ1Ijoicm9iZXJ0b3NhbGF6YXJtYXJxdWV6IiwiYSI6ImNsMXdyejhqNTJ6bWozY3BjaTlkMHBqaTQifQ.e6d_r7x8mhfRqOKlK3QEQg'
+            }
+        });
+        const resp = await consulta.get();
+        console.log(resp.data);
         return []
     }
 }
